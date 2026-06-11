d1e4ead9fbf0b09d5d5ef2716c5d3e860dc50578
woot000 <woot000@woot000.com>
2026-06-11 14:41:15 -0500

    suite: add ability to insert custom environment variables via text files
    Allows the user to create text files defining additional environment variables to be appended during build time per repo. Not a perfect implementation, as whenever the suite fails on configure/cmake/meson/rust the user will have to exit twice due to the suite invoking a subshell to temporarily define the user's environment variables

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1994,9 +1994,12 @@ if [[ $x264 != no ]] ||
                 [[ -f config.mak ]] && log "distclean" make distclean
                 do_uninstall "${_check[@]}"
                 create_build_dir
-                log configure ../configure --prefix="$LOCALDESTDIR/opt/lightffmpeg"
+                get_custom_flags userflags
+                ( [[ -n $userflags ]] && export "${userflags[@]}";
+                    log configure ../configure --prefix="$LOCALDESTDIR/opt/lightffmpeg" )
                 do_make install-lib
                 do_checkIfExist
+                unset userflags
             fi
             cd_safe "$LOCALBUILDDIR"/x264-git
         else
@@ -2025,15 +2028,17 @@ if [[ $x264 != no ]] ||
         do_uninstall "${_check[@]}"
         check_custom_patches
         create_build_dir
+        get_custom_flags userflags
         extra_script pre configure
-        PKGCONFIG="$PKG_CONFIG" CFLAGS="${CFLAGS// -O2 / }" \
-            log configure ../configure "${extracommands[@]}"
+        ( [[ -n $userflags ]] && export "${userflags[@]}";
+            PKGCONFIG="$PKG_CONFIG" CFLAGS="${CFLAGS// -O2 / }" \
+            log configure ../configure "${extracommands[@]}" )
         extra_script post configure
         do_make
         do_makeinstall
         do_checkIfExist
         PKG_CONFIG_PATH=$old_PKG_CONFIG_PATH
-        unset extracommands x264_build old_PKG_CONFIG_PATH
+        unset extracommands userflags x264_build old_PKG_CONFIG_PATH
     fi
     unset _bitdepth
 else
@@ -2056,14 +2061,17 @@ if [[ ! $x265 = n ]] && do_vcs "$SOURCE_REPO_X265"; then
 
     do_x265_cmake() {
         do_print_progress "Building $1" && shift 1
+        get_custom_flags userflags
         extra_script pre cmake
-        log "cmake" cmake "$(get_first_subdir -f)/source" -G Ninja \
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-        -DCMAKE_INSTALL_PREFIX="$LOCALDESTDIR" -DBIN_INSTALL_DIR="$LOCALDESTDIR/bin-video" \
-        -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DHIGH_BIT_DEPTH=ON \
-        -DENABLE_HDR10_PLUS=ON $xpsupport -DCMAKE_CXX_COMPILER="$LOCALDESTDIR/bin/${CXX#ccache }.bat" \
-        -DCMAKE_TOOLCHAIN_FILE="$LOCALDESTDIR/etc/toolchain.cmake" "$@"
+        ( [[ -n $userflags ]] && export "${userflags[@]}";
+            log "cmake" cmake "$(get_first_subdir -f)/source" -G Ninja \
+            -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+            -DCMAKE_INSTALL_PREFIX="$LOCALDESTDIR" -DBIN_INSTALL_DIR="$LOCALDESTDIR/bin-video" \
+            -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DHIGH_BIT_DEPTH=ON \
+            -DENABLE_HDR10_PLUS=ON $xpsupport -DCMAKE_CXX_COMPILER="$LOCALDESTDIR/bin/${CXX#ccache }.bat" \
+            -DCMAKE_TOOLCHAIN_FILE="$LOCALDESTDIR/etc/toolchain.cmake" "$@" )
         extra_script post cmake
+        unset userflags
         do_ninja
     }
     [[ $standalone = y || $av1an = y ]] && cli=-DENABLE_CLI=ON
@@ -2830,9 +2838,11 @@ if [[ $mpv != n ]] && pc_exists libavcodec libavformat libswscale libavfilter; t
             _check+=(bin-global/mujs.exe)
             sed -i "s;-lreadline;$($PKG_CONFIG --libs readline);g" Makefile
         fi
+        get_custom_flags userflags
         extra_script pre make
-        TEMP="${TEMP:-/tmp}" CPATH="${CPATH:-}" log "make" "$(command -v make)" \
-            "${mujs_targets[@]}" prefix="$LOCALDESTDIR" bindir="$LOCALDESTDIR/bin-global"
+        ( [[ -n $userflags ]] && export "${userflags[@]}";
+            TEMP="${TEMP:-/tmp}" CPATH="${CPATH:-}" log "make" "$(command -v make)" \
+            "${mujs_targets[@]}" prefix="$LOCALDESTDIR" bindir="$LOCALDESTDIR/bin-global" )
         extra_script post make
         extra_script pre install
         [[ $standalone != n ]] && do_install build/release/mujs "$LOCALDESTDIR/bin-global"
@@ -2842,7 +2852,7 @@ if [[ $mpv != n ]] && pc_exists libavcodec libavformat libswscale libavfilter; t
         extra_script post install
         grep_or_sed "Requires.private:" "$LOCALDESTDIR/lib/pkgconfig/mujs.pc" \
             's;Version:.*;&\nRequires.private: readline;'
-        unset mujs_targets
+        unset mujs_targets userflags
         do_checkIfExist
     fi
 
